28dff1949b2f81ed381119161e5aec34688cb674
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-15 13:25:32 -0700

    reassign group letters

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group
-    attr_accessor :age, :letter
+    attr_accessor :age, :letter, :code
     attr_reader :campers
     @@counter = 0
     @@all = []
@@ -11,16 +11,48 @@ class Group
       @@all << self
     end
 
+    def campers
+      Camper.all.select{|c|c.group_id2 == self.letter }
+    end
+
     def self.all
       @@all
     end
 
     def size
-      @campers.try(:length)
+      campers.try(:length)
     end
 
     def self.find_by_letter(let)
       all.detect{|g| g.letter == let}
+    end
+
+    def self.new_code(code)
+      #"A" = 65 "Z" = 90
+      if code[-1].ord < 90
+        new_code = code.split("").map{|c| (c.ord + 1).chr}.join
+      else
+        new_length = code.length + 1
+        new_code = "A" * new_length
+      end
+      new_code
+    end
+    #turn this into a generator later
+
+    def code=(code)
+      self.campers.each do |c|
+        c.group_id2 = code
+      end
+      @code = code
+    end
+
+    def self.assign_codes
+      @@all.delete_if{|g| g.size == 0}
+      code = "A"
+      @@all.each do |g|
+        g.code = code
+        code = new_code(code)
+      end
     end
 
   end
