91c9fedf30f511bbdd04448ceeaa9573933fb9ee
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-14 21:43:07 -0700

    fix initialize arguments

--- app/models/parser.rb
+++ app/models/parser.rb
@@ -3,7 +3,7 @@ class Parser
     @@codes = %w[A B C D E F G H I J K L M N O P Q R S T U V W Y Z AA BB CC DD EE FF GG HH II JJ KK LL MM NN OO PP QQ RR SS TT UU VV WW YY ZZ AAA BBB CCC DDD EEE FFF GGG HHH III JJJ KKK LLL MMM NNN OOO PPP QQQ RRR SSS TTT UUU VVV WWW YYY ZZZ AAAA BBBB CCCC DDDD EEEE FFFF GGGG HHHH IIII JJJJ KKKK LLLL MMMM NNNN OOOO PPPP QQQQ RRRR SSSS TTTT UUUU VVVV WWWW YYYY ZZZZ AAAAA BBBBB CCCCC DDDDD EEEEE FFFFF GGGGG HHHHH IIIII JJJJJ KKKKK LLLLL MMMMM NNNNN OOOOO PPPPP QQQQQ RRRRR SSSSS TTTTT UUUUU VVVVV WWWWW YYYYY ZZZZZ AAAAAA BBBBBB CCCCCC DDDDDD EEEEEE FFFFFF GGGGGG HHHHHH IIIIII JJJJJJ KKKKKK LLLLLL MMMMMM NNNNNN OOOOOO PPPPPP]
 
 
-    def initialize(data, path)
+    def initialize(data:, path:)
         @data = data
         @path = path
     end
@@ -44,6 +44,7 @@ class Parser
         end
 
         #instantiate new camper
+        #need to fix this to make it more abstract and use the passed in row headers
         Camper.new({name_used: line["name used"], first: line["first"], last: line["last"], age: line["age"], grade: line["grade"], session: line["session"], city: line["city"], prev_grade: line["prev_grade"], school: line["school"], birthdate: line["birthdate"], tribe: line["tribe"], sibling_tribe: line["sibling_tribe"], tenure: line["years at ozark"], rid: line["rid2"], prev_cab: line["prev_cab"], past_camper_rating: line["past_camper_rating"], balance: line["balance"], parent_notes: line["parent_notes"], discount: line["discount"], campmom_med_discount: line["campmom_med_discount"], admin_notes: line["admin_notes"], req1: req1, req1_city: req1_city, req2: req2, req2_city: req2_city, ofa_group: line["ofa_group"]})
       end
     end
@@ -56,7 +57,7 @@ class Parser
           csv << [c.session, c.name_used, "", c.age, c.grade, c.prev_grade, c.first, c.last, c.city.name, c.school, c.birthdate, c.tribe, c.sibling_tribe, c.tenure, c.group_id_1, group2, c.rid, c.prev_cab, c.past_camper_rating, c.balance, c.discount, c.campmom_med_discount, c.parent_notes, c.admin_notes, "", "", "#{c.req1} (#{c.req1_city})", "", "", "#{c.req2} (#{c.req2_city})", "", "", c.ofa_group]
         end
       end
-      File.write("boys_requests.csv", data)
+      File.write(@path, data)
     end
 
   end
