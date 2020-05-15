258aca308814e6cb3bde79eac30143ecc57314c5
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-14 21:21:49 -0700

    create initial get and post routes

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < Sinatra::Base
+    configure do
+      set :public_folder, 'public'
+      set :views, 'app/views'
+    end
+
+    # code actions here!
+
+    get '/' do
+        erb :'/index'
+    end
+
+    post '/upload' do
+        csv_data = CSV.read(params[:camp][:upload][:tempfile], headers:true, encoding:'iso-8859-1:utf-8')
+        filename = params[:camp][:upload][:filename]
+        binding.pry
+        puts 'hi'
+    end
+
+  end
