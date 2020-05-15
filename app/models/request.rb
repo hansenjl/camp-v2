75a2a1af05df57701d84b2b8c8198028d40fc401
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-15 13:26:24 -0700

    move sanitization of req data to req model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request
-    attr_accessor :first, :last, :city, :met, :camper, :val
+    attr_accessor :first, :last, :city, :met, :camper, :value
 
     def initialize(req, city, camper, value)
       @met = false
@@ -10,6 +10,24 @@ class Request
       @city = city
       @value = value
       @camper = camper
+    end
+
+    def self.new_from_string(string, camper, value)
+      if !string.nil? && !string.strip.empty?
+        request1 =  string.split("(")
+        req1_city = request1[-1].chomp(")") if request1[-1] != nil
+        city_obj = City.find_or_create_by_name(req1_city.downcase) if req1_city
+        if request1.length < 3
+          req1 = request1[0].strip.upcase
+        else
+          r = request1[1].split(")")
+          req1 = request1[0].strip.upcase + " " + r[-1].strip.upcase
+        end
+      else
+        req1 = nil
+        city_obj = nil
+      end
+      Request.new(req1, city_obj, camper, value)
     end
 
   end
