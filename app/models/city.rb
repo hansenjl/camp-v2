834555cdc54113effa52fa49357a502df210f7d3
hansenjl <jenn.leigh.hansen@gmail.com>
2020-05-14 21:34:21 -0700

    import past city code

---
+++ app/models/city.rb
@@ -0,0 +1,35 @@
+class City
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+      @name = name.downcase if name
+      @campers = []
+      @@all << self
+      self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def campers
+      @campers
+    end
+
+    def self.report
+      rep = {}
+      self.all.each do |c|
+        rep[c.name] = c.campers.length
+      end
+      rep
+    end
+
+    def self.find_or_create_by_name(name)
+      self.find_by_name(name) ? self.find_by_name(name) : self.new(name)
+    end
+
+    def self.find_by_name(name)
+        all.detect{|c| c.name == name.downcase if name}
+    end
+  end
